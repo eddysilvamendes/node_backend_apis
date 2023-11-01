fc0d8eb283abcb1bd2e89be14784502187e19060
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-01 19:18:38 -0100

    feat(categories.js): add Categories model to define the schema for categories in the database

---
+++ foodly_backend_api/models/Categories.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const CategorieSchema = new mongoose.Schema({
+   title: {type: String, required:true},
+   value: {type: String, required:true},
+   imageUrl: {type: String, required:true},
+},{timestamps:true});
+
+module.exports = mongoose.model('Category',CategorieSchema)
