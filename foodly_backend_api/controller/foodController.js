14598376c59d751e0bab2bcfaef779c6c0951518
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-02 17:01:53 -0100

    feat(foodController.js): add addFood function to handle adding a new food item
    The addFood function is added to the foodController.js file. This function is responsible for creating a new instance of the Food model and saving it to the database. If the save operation is successful, a response with status 201 and a success message is sent. If there is an error during the save operation, a response with status 500 and the error message is sent.

--- foodly_backend_api/controller/foodController.js
+++ foodly_backend_api/controller/foodController.js
@@ -1,2 +1,16 @@
 //Create a instance of model
-const User = require('../models/Food')
+const Food = require('../models/Food')
+
+
+module.exports = {
+    addFood:async (request,response) => {
+        const newFood = new Food(request.body)
+
+        try {
+            await newFood.save();
+            response.status(201).json({status:true,message:'Food added successfully'})
+        } catch (error) {
+            response.status(500).json({status:false,message:error.message})
+        }
+    }
+}
