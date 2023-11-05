af64f1bdb4d5f5a754d43c30b5b38f665cf7d9b1
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-05 00:10:37 -0100

    feat(cartController.js): implement addProductToCart function
    The addProductToCart function has been implemented in the cartController.js file. This function allows a user to add a product to their cart. It takes in the user's ID, product ID, total price, and quantity as parameters.
    
    Inside the function, it checks if the product already exists in the cart for the user. If it does, it increments the quantity and total price of the existing product. If it doesn't, it creates a new cart item with the provided details.
    
    After adding the product to the cart, the function returns the updated count of items in the cart. If there is an error, it returns an error message.

--- foodly_backend_api/controller/cartController.js
+++ foodly_backend_api/controller/cartController.js
@@ -1,10 +1,37 @@
 const Cart = require("../models/Cart");
 
 module.exports = {
-  addProductToCart: async (reques, response) => {},
-  removeProductFromCart: async (reques, response) => {},
-  fetchUserCart: async (reques, response) => {},
-  clearUserCart: async (reques, response) => {},
-  getCartCount: async (reques, response) => {},
-  decrimentProductQty: async (reques, response) => {},
+  addProductToCart: async (request, response) => {
+    const user_id = request.user.id;
+    const { product_id, total_price, quantity } = request.boddy;
+    let count;
+
+    try {
+      const existing_product = await Cart.findOne({ user_id, product_id });
+      count = await Cart.countDocuments({ user_id });
+      if (existing_product) {
+        (existing_product.quantity += 1),
+          (existing_product.total_price += total_price),
+          await existing_product.save();
+      } else {
+        const newCart = new Cart({
+          user_id: user_id,
+          product_id: request.body.product_id,
+          additives: request.body.additives,
+          total_price: request.body.total_price,
+          quantity: request.body.quantity,
+        });
+        await newCart.save();
+        count = await Cart.countDocuments({ user_id });
+      }
+      response.status(200).json({ status: true, count: count });
+    } catch (error) {
+      response.status(200).json({ status: false, error: error.message });
+    }
+  },
+  removeProductFromCart: async (request, response) => {},
+  fetchUserCart: async (request, response) => {},
+  clearUserCart: async (request, response) => {},
+  getCartCount: async (request, response) => {},
+  decrimentProductQty: async (request, response) => {},
 };
