c73857f07562fa35448f6e6b4ffc967c35ff7ddf
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-05 14:40:01 -0100

    feat(addressController.js, Address.js): add Address model and controller to handle user addresses
    The Address model and controller have been added to the project. The Address model defines the structure of an address with fields such as user_id, address_line_1, city, state, district, postalCode, default, country, and delivery_instructions. The Address controller will handle CRUD operations related to user addresses.

---
+++ foodly_backend_api/models/Address.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const AddressSchema = new mongoose.Schema(
+  {
+    user_id: { type: String, required: true },
+    address_line_1: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    disctrict: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    default: { type: Boolean, default: true },
+    country: { type: String, required: true },
+    delivery_instructions: { type: String },
+  },
+  { timestamps: false }
+);
+
+module.exports = mongoose.model("Address", AddressSchema);
