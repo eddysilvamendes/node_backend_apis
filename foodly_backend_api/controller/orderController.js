7698ec39a15bdda180bdfededd5d428cac216036
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-05 19:09:31 -0100

    feat(orderController.js): add placeOrder function to handle placing orders
    feat(orderController.js): add getOrderDetails function to retrieve order details
    
    feat(orderController.js): add getUserOrder function to retrieve user's orders
    
    feat(orderController.js): add rateOrders function to handle rating orders
    
    feat(orderController.js): add updateOrderStatus function to update order status
    
    feat(orderController.js): add updatePaymentStatus function to update payment status

--- foodly_backend_api/controller/orderController.js
+++ foodly_backend_api/controller/orderController.js
@@ -1,1 +1,50 @@
 const Order = require("../models/Orders");
+
+module.exports = {
+  placeOrder: async (request, response) => {
+    const order = new Order(request.body);
+    try {
+      await order.save();
+      response
+        .status(201)
+        .json({ status: true, message: "Order placed successfully" });
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  getOrderDetails: async (request, response) => {
+    const order_id = request.params.id;
+    try {
+      const order = await Order.findById(order_id)
+        .populate({
+          path: "user_id",
+          select: "name email phone",
+        })
+        .populate({
+          path: "delivery_address_id",
+          select: "address_line_1 city state postal_code",
+        })
+        .populate({
+          path: "restaurant_id",
+          select: "name location",
+        })
+        .populate({
+          path: "driver_id",
+          select: "name phone",
+        });
+      if (order) {
+        response.status(200).json(order);
+      } else {
+        response
+          .status(404)
+          .json({ status: false, message: "Order not found!" });
+      }
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  getUserOrder: async (request, response) => {},
+  rateOrders: async (request, response) => {},
+  updateOrderStatus: async (request, response) => {},
+  updatePaymentStatus: async (request, response) => {},
+};
