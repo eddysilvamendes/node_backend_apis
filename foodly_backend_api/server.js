c5bbaa90ce63f2c8877c4f7855c1d1ae0428dac3
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-05 15:18:51 -0100

    feat(server.js): add support for address router to handle address-related API endpoints

--- foodly_backend_api/server.js
+++ foodly_backend_api/server.js
@@ -10,6 +10,7 @@ const restaurantRouter = require("./routes/restaurant");
 const categoryRouter = require("./routes/category");
 const foodRouter = require("./routes/food");
 const cartRouter = require("./routes/cart");
+const addressRouter = require("./routes/address");
 
 dotenv.config();
 
@@ -37,6 +38,7 @@ app.use("/api/restaurant", restaurantRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/food", foodRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
 app.listen(process.env.PORT || port, () =>
   console.log(`Foodly Backend API app listening on port ${process.env.PORT}!`)
 );
