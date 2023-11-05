a8aed3f4b26a3ada8af5489184d46a933faebc72
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-05 15:12:16 -0100

    feat(addressController.js): add CRUD operations for addresses
    feat(addressController.js): add createAddress function to create a new address for a user
    
    feat(addressController.js): add deleteAddress function to delete an address by its ID
    
    feat(addressController.js): add getDefaultAddress function to get the default address for a user
    
    feat(addressController.js): add getUserAddresses function to get all addresses for a user
    
    feat(addressController.js): add updateAddress function to update an address by its ID
    
    feat(addressController.js): add setDefaultAddress function to set an address as the default address for a user
    
    fix(Address.js): fix typo in postal_code field name

---
+++ foodly_backend_api/controller/addressController.js
@@ -0,0 +1,127 @@
+const Address = require("../models/Address");
+
+module.exports = {
+  createAddress: async (request, response) => {
+    const address = new Address({
+      user_id: request.user.id,
+      address_line_1: request.body.address_line_1,
+      city: request.body.city,
+      state: request.body.state,
+      disctrict: request.body.disctrict,
+      postal_code: request.body.postal_code,
+      country: request.body.country,
+      delivery_instructions: request.body.delivery_instructions,
+      default: request.body.default,
+    });
+
+    try {
+      if (request.body.default) {
+        await Address.updateMany(
+          {
+            user_id: request.user.id,
+          },
+          { default: false }
+        );
+      }
+      await address.save();
+      response
+        .status(201)
+        .json({ status: true, message: "Address created successfully!" });
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  deleteAddress: async (request, response) => {
+    const address_id = request.params.id;
+    try {
+      const address = await Address.findById(address_id);
+      if (!address) {
+        return response
+          .status(404)
+          .json({ status: false, message: "Address not found!" });
+      }
+      await Address.findByIdAndDelete(address_id);
+      response
+        .status(500)
+        .json({ status: true, message: "Address deleted successfully" });
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  getDefaultAddress: async (request, response) => {
+    const user_id = request.user.id;
+    try {
+      const default_address = await Address.findOne({ user_id, default: true });
+      response.status(200).json(default_address);
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  getUserAddresses: async (request, response) => {
+    const user_id = request.user.id;
+    try {
+      const address = await Address.find({ user_id });
+      response.status(200).json(address);
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  updateAddress: async (request, response) => {
+    const address_id = request.params.id;
+    const address = new Address({
+      user_id: request.user.id,
+      address_line_1: request.body.address_line_1,
+      city: request.body.city,
+      state: request.body.state,
+      disctrict: request.body.disctrict,
+      postal_code: request.body.postal_code,
+      country: request.body.country,
+      delivery_instructions: request.body.delivery_instructions,
+      default: request.body.default,
+    });
+
+    try {
+      if (request.body.default) {
+        await Address.updateMany(
+          { user_id: request.user.id },
+          { default: false }
+        );
+      }
+      await Address.findByIdAndUpdate(address_id, address, { new: true });
+      response
+        .status(201)
+        .json({ status: true, message: "Address updated successfully!" });
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+  setDetaultAddress: async (request, response) => {
+    const address_id = request.body.address_id;
+    const user_id = request.user.id;
+
+    try {
+      await Address.updateMany(
+        {
+          user_id,
+        },
+        { default: false }
+      );
+      const updated_address = await Address.findByIdAndUpdate(
+        address_id,
+        { default: true },
+        { new: true }
+      );
+      if (updated_address) {
+        response
+          .status(200)
+          .json({ status: true, message: "Address updated successfully" });
+      } else {
+        response
+          .status(404)
+          .json({ status: false, message: "Address not found" });
+      }
+    } catch (error) {
+      response.status(500).json({ status: false, message: error.message });
+    }
+  },
+};
--- foodly_backend_api/models/Address.js
+++ foodly_backend_api/models/Address.js
@@ -7,7 +7,7 @@ const AddressSchema = new mongoose.Schema(
     city: { type: String, required: true },
     state: { type: String, required: true },
     disctrict: { type: String, required: true },
-    postalCode: { type: String, required: true },
+    postal_code: { type: String, required: true },
     default: { type: Boolean, default: true },
     country: { type: String, required: true },
     delivery_instructions: { type: String },
