79b72edee1164f00b9ec0abcea5ebce39902611d
eddysilvamendes <eddysilvamendes19@gmail.com>
2023-11-05 18:57:13 -0100

    feat(orderController.js, Orders.js): add OrderController and Order model to handle orders in the backend API
    The OrderController.js file is a new file that has been added to the controller directory. It contains the logic for handling orders in the backend API.
    
    The Orders.js file has been modified to include the necessary schema for the Order model. It includes fields such as user_id, order_item, order_total, delivery_fee, grand_total, delivery_address_id, payment_method, payment_status, order_status, order_date, restaurant_id, driver_id, rating, feedback, promo_code, discount_amount, and notes.
    
    These changes were made to implement the functionality for managing orders in the backend API.

--- foodly_backend_api/models/Orders.js
+++ foodly_backend_api/models/Orders.js
@@ -1,16 +1,49 @@
 const mongoose = require("mongoose");
 
-const OrderItemSchema = new mongoose.Schema(
+const OrderItemSchema = new mongoose.Schema({
+  food_id: { type: mongoose.Schema.Types.ObjectId, ref: "Food" },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  additives: { type: Array },
+  instructions: { type: String, default: "" },
+});
+
+const OrderSchema = new mongoose.Schema(
   {
-    food_id: { type: mongoose.Schema.Types.ObjectId, ref: "Food" },
-    quantity: { type: Number, required: true },
-    price: { type: Number, required: true },
-    additives: { type: Array },
-    instructions: { type: String, default: "" },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    order_item: [OrderItemSchema],
+    order_total: { type: Number, required: true },
+    delivery_fee: { type: Number, required: true },
+    grand_total: { type: Number, required: true },
+    delivery_address_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+    },
+    payment_method: { type: String },
+    payment_status: {
+      type: String,
+      default: "Pending",
+      enum: ["Pending", "Completed", "Faild"],
+    },
+    order_status: {
+      type: String,
+      default: "Placed",
+      enum: ["Placed", "Preparing", "Out for delivery", "Delivery"],
+    },
+    order_date: { type: Date, default: Date.now },
+    restaurant_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+    driver_id: { type: mongoose.Schema.Types.ObjectId, ref: "Driver" },
+    rating: { type: Number, min: 1, max: 5 },
+    feedback: { type: String },
+    promo_code: { type: String },
+    discount_amount: { type: Number },
+    notes: { type: String },
   },
   { timestamps: true }
 );
-
-const OrderSchema = new mongoose.Schema({}, { timestamps: true });
 
 module.exports = mongoose.model("Order", OrderSchema);
